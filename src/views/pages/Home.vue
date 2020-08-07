c1bdfecd4bb7cb2fcf7395ca941e73282119ec71
Saravanan Kalimuthu <sarvaruna@outlook.com>
2020-08-08 01:49:35 +0530

    Home changes

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,30 +1,73 @@
 <template>
-<v-container id="home" tag="section" fluid class="fill-height">
-  <v-row class="fill-height" align="center" justify="center" align-content="center">
-    <v-col>
-      <v-img contain src="../../assets/first.jpg">
-        <v-row class="fill-height" align="center" justify="end" align-content="center">
-          <v-col cols="8" align-self="center">
-            <v-row align="center" justify="center" align-content="center">
-              <v-col cols="9">
-                <div class="d-flex flex-column py-2">
-                  <p class="font-weight-bold text--primary text-h1">OOP'S</p>
-                </div>
-              </v-col>
-            </v-row>
-            <v-row align="center" justify="center" align-content="center">
-              <v-col cols="9">
-                <p class="text--primary">
-                  Yup that's right am the person who fix the problem that you don't know you have,
-                  in a way you don't understand .
-                </p>
-              </v-col>
-            </v-row>
+  <v-container id="home" tag="section" fluid class="fill-height">
+    <v-row class="fill-height" align="center" justify="center" align-content="center">
+      <v-col align-self="center">
+        <!-- //Normal screen -->
+        <v-img contain src="../../assets/first.jpg">
+          <v-row
+            class="fill-height d-none d-sm-flex"
+            align="end"
+            justify="end"
+            align-content="center"
+          >
+            <v-col cols="8" align-self="center">
+              <div>
+                <v-row align="center" justify="center" align-content="center">
+                  <v-col cols="9">
+                    <div class="d-flex flex-column pt-2 border-radius">
+                      <p
+                        class="font-weight-bold text-left text--primary text-lg-h1 text-md-h1"
+                      >OOP'S</p>
+                    </div>
+                  </v-col>
+                </v-row>
+                <v-row align="center" justify="center" align-content="center">
+                  <v-col cols="9">
+                    <p
+                      class="text--primary text-left text-lg-h6 text-md-h6 text-sm-body-2"
+                    >
+                      Yup that's right am the person who fix the problem that you don't know you have,
+                      in a way you don't understand .
+                    </p>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-col>
+          </v-row>
+        </v-img>
+        <!-- mobile Only -->
+        <v-row
+          class="d-flex d-sm-none"
+          align="end"
+          justify="end"
+          align-content="center"
+        >
+          <v-col cols="12" align-self="center">
+            <v-card color="secondary">
+              <v-row align="center" justify="center" align-content="center">
+                <v-col cols="9">
+                  <div class="d-flex flex-column pt-2 border-radius">
+                    <p
+                      class="font-weight-bold text-left text--primary text-h1"
+                    >OOP'S</p>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row align="center" justify="center" align-content="center">
+                <v-col cols="9">
+                  <p
+                    class="text--primary text-left text-h6"
+                  >
+                    Yup that's right am the person who fix the problem that you don't know you have,
+                    in a way you don't understand .
+                  </p>
+                </v-col>
+              </v-row>
+            </v-card>
           </v-col>
         </v-row>
-      </v-img>
-    </v-col>
-  </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
@@ -34,3 +77,5 @@ export default {
   }),
 };
 </script>
+<style scoped>
+</style>
--- src/views/pages/Skills.vue
+++ src/views/pages/Skills.vue
@@ -1,11 +1,28 @@
 <template>
-  <v-row class="fill-height" justify="space-around">
-    <v-col>
-      <v-sheet class="fill-height pa-12 mx-auto" color="grey lighten-3" elevation="5">
-
-      </v-sheet>
-    </v-col>
-  </v-row>
+  <v-container id="skills" tag="section" fluid class="fill-height">
+    <v-row class="fill-height" align="center" justify="center" align-content="center">
+      <v-col cols="12" align-self="center">
+        <v-row no-gutters align="center" justify="center" align-content="center">
+          <p class="text--primary text-h2 my-0 py-1 px-3">🤔 LETS SEE WHAT I KNOW!</p>
+        </v-row>
+        <v-row no-gutters class="mt-12" align="center" justify="center" align-content="center">
+          <v-col class="mt-6" cols="12" align-self="center">
+            <v-card color="red" class="mx-auto mt-6">
+              <v-container>
+                <v-row no-gutters align="center" justify="center" align-content="center">
+                  <v-col class="px-7" cols="12" md="6" lg="6" align-self="center">
+                    <v-row>
+                      
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
